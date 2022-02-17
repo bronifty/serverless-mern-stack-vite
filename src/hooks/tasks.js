69e4ff7347064f82ddde3e01ceb6804ed9e0d446
bronifty <bronifty@gmail.com>
2022-02-17 18:31:35 -0500

    .

--- src/hooks/tasks.js
+++ src/hooks/tasks.js
@@ -17,31 +17,36 @@ import { useQuery, useMutation, useQueryClient } from 'react-query';
 
 const fetcher = async (options) => {
   let res;
-  if (options.body) {
-    res = await fetch(
-      `${options.id ? '/api/v1/tasks/' + options.id : '/api/v1/tasks'}`,
-      {
-        method: options.method,
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify(options.body),
-      }
-    );
-    return res.json();
-  } else {
-    res = await fetch(
-      `${options.id ? '/api/v1/tasks/' + options.id : '/api/v1/tasks'}`,
-      {
-        method: options.method,
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        },
-      }
-    );
-    return res.json();
+  try {
+    if (options.body) {
+      res = await fetch(
+        `${options.id ? '/api/v1/tasks/' + options.id : '/api/v1/tasks'}`,
+        {
+          method: options.method,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify(options.body),
+        }
+      );
+      return res.json();
+    } else {
+      res = await fetch(
+        `${options.id ? '/api/v1/tasks/' + options.id : '/api/v1/tasks'}`,
+        {
+          method: options.method,
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+        }
+      );
+      return res.json();
+    }
+  } catch (error) {
+    console.log({ error });
+    return res.json(error);
   }
 };
 
