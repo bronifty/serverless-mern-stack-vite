bb1c14ddd50f0976812ba024500b49d59b70b663
bronifty <bronifty@gmail.com>
2022-02-17 18:56:10 -0500

    .

--- utils/db/connect.js
+++ utils/db/connect.js
@@ -1,13 +1,39 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+// const uri = process.env.MONGO_URI;
+// const db =
+//   global.db ||
+//   mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   });
+// if (process.env.NODE_ENV === 'development') global.db = db;
+// module.exports = db;
+
 const mongoose = require('mongoose');
 require('dotenv').config();
 const uri = process.env.MONGO_URI;
-const db =
-  global.db ||
-  mongoose.connect(uri, {
+let db;
+
+if (process.env.NODE_ENV === 'production') {
+  db = mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   });
-if (process.env.NODE_ENV === 'development') global.db = db;
+} else {
+  if (!global.__db) {
+    global.__db = mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+  }
+  db = global.__db;
+}
 module.exports = db;
+// export { db };
