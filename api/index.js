c08786dd3c61b600a49046b955c98238843d1cf7
bronifty <bronifty@gmail.com>
2022-02-17 20:06:16 -0500

    .

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const app = express();
 const tasks = require('../utils/routes/tasks');
 require('../utils/db/connect');
 const cors = require('cors');
-// middleware
 app.use(express.json());
 app.use(express.static('dist'));
 app.use(
@@ -11,27 +10,5 @@ app.use(
     origin: '*',
   })
 );
-// routes
 app.use('/api/v1/tasks', tasks);
-
-// app.get('/api/v1/tasks')
-// app.post('/api/v1/tasks')
-// app.get('/api/v1/tasks/:id')
-// app.patch('/api/v1/tasks/:id')
-// app.delete('/api/v1/tasks/:id')
-
-app.get('/', (req, res) => {
-  res.send('Task Manager App');
-});
-
-// const PORT = 3000;
-// connectDb(process.env.MONGO_URI)
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log(`server listening on port ${PORT}...`);
-//     });
-//     console.log('connected to mongoDB');
-//   })
-//   .catch((err) => console.log(err));
-
 module.exports = app;
