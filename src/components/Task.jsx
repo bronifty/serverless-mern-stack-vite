981b0ebdb499387e3f1ff557e048a5507f84bfed
bronifty <bronifty@gmail.com>
2022-02-20 00:06:14 -0500

    .

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import { FaTrashAlt, FaRegCheckCircle, FaRegEdit } from 'react-icons/fa';
 import { useDeleteOne } from '../hooks';
+import { useQueryClient } from 'react-query';
 
 export const Task = ({ completed, _id: taskID, name }) => {
+  const queryClient = useQueryClient();
   const { mutate: deleteOne } = useDeleteOne();
   return (
     <div className={`single-task ${completed && 'task-completed'}`}>
@@ -15,7 +17,15 @@ export const Task = ({ completed, _id: taskID, name }) => {
         {name}
       </h5>
       <div className='task-links'>
-        <Link to={`/tasks/${taskID}`} className='edit-link'>
+        <Link
+          to={`/tasks/${taskID}`}
+          className='edit-link'
+          onMouseOver={() => {
+            queryClient.prefetchQuery(['fetchOne', taskID], () => fetchAll(), {
+              staleTime: Infinity,
+            });
+          }}
+        >
           <FaRegEdit />
         </Link>
 
